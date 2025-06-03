78d1944433b4e97557bc765e07fdc0b540483427
KevinJHP19 <kevinjordanhuaman@gmail.com>
2025-06-03 19:05:35 +0200

    ahora si imagenes populares hecha

--- src/miscomponentes/Peliculas.js
+++ src/miscomponentes/Peliculas.js
@@ -3,10 +3,6 @@ import React from 'react';
 export default function Peliculas({peliculas}){
     console.log( "Peliculas cargado")
     console.log(peliculas);
-    const contador= 0;
-
-    
-
     return (
         <div className='container mx-auto p-4'>
             <h1 className="text-2xl font-bold mb-4 text-center">Películas Populares</h1>
