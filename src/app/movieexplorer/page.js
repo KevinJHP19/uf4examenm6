945e66e6aeaf881ffe56d754b9ce0b9569439056
KevinJHP19 <kevinjordanhuaman@gmail.com>
2025-06-03 18:30:34 +0200

    llamando a las peliculas populares a la  consola

--- src/app/movieexplorer/page.js
+++ src/app/movieexplorer/page.js
@@ -1,6 +1,13 @@
+import Peliculas from "@/miscomponentes/Peliculas";
 export default function MovieExplorer() {
     console.log( "MovieExplorer cargado")
     return (
-        <h1>MovieExplorer</h1>
+        <div>
+            <h1>MovieExplorer</h1>
+        <Peliculas />
+        </div>
+        
+
+        
     )
 }
